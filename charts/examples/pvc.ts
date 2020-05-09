65cd26be27c774a52929cc159c551f842b59feab
Rafael Ribeiro <bribeiro@neoception.com>
2020-05-09 22:44:15 +0100

    added some examples

---
+++ charts/examples/pvc.ts
@@ -0,0 +1,20 @@
+import { App, Chart } from 'cdk8s';
+import { Construct } from 'constructs';
+import { LocalPathPVC } from '../../lib/local-path-pvc';
+
+export class PVC extends Chart {
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
+
+    new LocalPathPVC(this, {
+      accessModes: ['ReadWriteMany'],
+      name: 't',
+      namespace: 'local-path-test',
+      size: '10Gi'
+    });
+  }
+}
+
+const app = new App();
+new PVC(app, 'pvcteste');
+//app.synth();
