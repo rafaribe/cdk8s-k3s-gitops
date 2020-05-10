d8f423828d4456e8b593523a10d80e1782d9bfc6
Rafael Ribeiro <bribeiro@neoception.com>
2020-05-10 23:17:47 +0100

    added some comments to flux bootstrap script

--- scripts/bootstrap-flux.sh
+++ scripts/bootstrap-flux.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+echo '---------------------------'
+echo '👔Bootstrapping Flux'
+echo '---------------------------'
+echo '---------------------------'
+echo '🖥Switching Shell Context to k3s cluster'
+echo '---------------------------'
 kubectx k3s
 
+echo '---------------------------'
+echo '👔Creating Flux Namespace'
+echo '---------------------------'
 kubectl create namespace flux
 # Specific Secret key that I have placed under ignore to not have public exposure, you should create your own or use fluxctl identity.
+
+echo '---------------------------'
+echo '🔑Applying Fluxs git key'
+echo '---------------------------'
 kubectl apply -f flux-git-deploy.yaml
 
 cd ../cluster/charts
+echo '---------------------------'
+echo '🔭Installing Flux'
+echo '---------------------------'
 kubectl apply -f flux.k8s.yaml
-
+# Helm Operator install
+echo '---------------------------'
+echo '⚓️Installing Helm Operator'
+echo '---------------------------'
 helm upgrade -i helm-operator fluxcd/helm-operator \
     --namespace flux \
     --set helm.versions=v3 \
@@ -17,5 +36,12 @@ helm upgrade -i helm-operator fluxcd/helm-operator \
     --set git.ssh.secretName=flux-git-deploy
 
 export FLUX_FORWARD_NAMESPACE=flux
-
+# Wait for flux to boot up
+echo '---------------------------'
+echo '⏱Waiting 60 secs for flux to boot up'
+echo '---------------------------'
+sleep 60
+echo '---------------------------'
+echo '✌️Flux sync with the git repository'
+echo '---------------------------'
 fluxctl sync
