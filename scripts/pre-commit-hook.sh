50036f8cd6de80187fb54dfc2fc0383e8aba28c0
Rafael Ribeiro <bribeiro@neoception.com>
2020-05-23 23:22:35 +0100

    changed pre commit hook

--- scripts/pre-commit-hook.sh
+++ scripts/pre-commit-hook.sh
@@ -1,17 +1,17 @@
 echo '-----------------------------'
 echo ' 🤩Pre commit hook started'
 echo '-----------------------------'
-echo '⚙️Importing CRDs'
-npm run import
-echo '-----------------------------'
-echo '💅🏻 Running Prettier on the code'
-npm run prettier
-echo '-----------------------------'
-echo '👀 Linting the code'
-npm run lint
-echo '-----------------------------'
-echo '😎Compile and Synth'
-npm run compile-synth
-echo '-----------------------------'
-echo '🏁 Pre-commit hook finished'
-git stage .
+##echo '⚙️Importing CRDs'
+##npm run import
+##echo '-----------------------------'
+#echo '💅🏻 Running Prettier on the code'
+#npm run prettier
+#echo '-----------------------------'
+#echo '👀 Linting the code'
+#npm run lint
+#echo '-----------------------------'
+#echo '😎Compile and Synth'
+#npm run compile-synth
+#echo '-----------------------------'
+#echo '🏁 Pre-commit hook finished'
+#git stage .
